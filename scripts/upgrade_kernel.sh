3c5c0fa71ad55883a960b9d1743bf73522d30c1a
Shabir Ahmed <shabirahmed@OSX-0591.local>
2020-05-05 15:43:46 -0500

    a new hope

--- scripts/upgrade_kernel.sh
+++ scripts/upgrade_kernel.sh
@@ -5,6 +5,8 @@ set -o nounset
 set -o errexit
 
 #sudo yum update -y kernel
-sudo amazon-linux-extras install kernel-ng
+#sudo amazon-linux-extras install kernel-ng
+sudo yum -y install kernel-4.14.133-113.112.amzn2.x86_64
+sudo grubby --set-default /boot/vmlinuz-4.14.133-113.112.amzn2.x86_64
 echo "rebooting... now"
 sudo reboot
